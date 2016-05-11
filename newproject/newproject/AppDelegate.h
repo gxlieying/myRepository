92e954c236a3060c708b2f9ce0ce20b141f2350b
lanou3g <lieyinggx@gmail.com>
2016-05-11 19:52:57 +0800

    htrshrgjdj

---
+++ newproject/newproject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newproject
+//
+//  Created by lanou3g on 16/5/11.
+//  Copyright © 2016年 郭旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
